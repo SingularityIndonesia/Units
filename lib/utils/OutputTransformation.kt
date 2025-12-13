414e3c1224bc51b1ae6883d3750a8635abe51d41
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-12-13 20:47:44 +0700

    feat(compose): extract text transformation

--- lib/utils/OutputTransformation.kt
+++ lib/utils/OutputTransformation.kt
@@ -1,12 +1,10 @@
-package utils
-
 import androidx.compose.foundation.text.input.OutputTransformation
 import androidx.compose.foundation.text.input.insert
 
 val DEFAULT_PHONE_OUTPUT_TRANSFORMATION = OutputTransformation {
     if (originalText.length > 3)
-        insert(3,"-")
+        insert(3, "-")
 
     if (originalText.length > 7)
-        insert(8,"-")
+        insert(8, "-")
 }
