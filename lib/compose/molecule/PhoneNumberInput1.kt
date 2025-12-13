3fa7a4d71cf3b8ac4bc6b6fb6b085e7192bf0380
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-12-13 13:45:53 +0700

    feat(compose): add phone input

---
+++ lib/compose/molecule/PhoneNumberInput1.kt
@@ -0,0 +1,89 @@
+package molecule
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldDecorator
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+private fun Preview() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        PhoneNumberInput(
+            modifier = Modifier.width(220.dp),
+            countryCode = "+62",
+            hint = "895 xxxx xxxx",
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number
+            )
+        )
+    }
+}
+
+@Composable
+fun PhoneNumberInput(
+    modifier: Modifier = Modifier,
+    state: TextFieldState = remember { TextFieldState() },
+    countryCode: String = "",
+    hint: String = "",
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default
+) {
+    BasicTextField(
+        modifier = modifier,
+        state = state,
+        keyboardOptions = keyboardOptions,
+        decorator = TextFieldDecorator { innerText ->
+            Row(
+                Modifier
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                if (countryCode.isNotEmpty()) {
+                    Text(countryCode)
+                    Spacer(
+                        modifier = Modifier.width(1.dp)
+                            .height(18.dp)
+                            .background(MaterialTheme.colorScheme.onSurface)
+                    )
+                }
+
+                Box {
+                    if (state.text.isEmpty()) {
+                        Text(
+                            text = hint,
+                            color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    }
+
+                    innerText()
+                }
+            }
+        },
+    )
+}
