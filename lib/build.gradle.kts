8365782d19dc35200468f93721c5bc993d0863b0
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-12-13 12:32:45 +0700

    chore: add ktor deps

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -56,6 +56,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.ktor.client)
         }
 
         commonTest.dependencies {
