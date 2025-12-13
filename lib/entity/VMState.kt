6ea0ad3272fd72cff9ca0bced4f37c01c339625e
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-12-13 13:26:43 +0700

    feat(entity): add VMState utils

--- lib/entity/VMState.kt
+++ lib/entity/VMState.kt
@@ -1,11 +1,32 @@
-sealed interface VMState<T : Any> {
-    object Idle : VMState<Nothing>
-    object Loading : VMState<Nothing>
-    data class Success<T : Any>(val data: T) : VMState<T>
-    data class Failed(val e: Exception) : VMState<Nothing>
+// region Lifter
+/**
+ * Override this error lifter to modify the error resolver
+ */
+var ErrorLifter = { e: Throwable ->
+    failed(Exception(e))
+}
+
+fun Throwable.liftError(): VMState<Nothing> {
+    return ErrorLifter(this)
+}
+// endregion
+
+// region Utils
+suspend fun <T : Any> exec(bloc: suspend () -> T): VMState<T> {
+    return runCatching { bloc.invoke() }
+        .map { success(it) }
+        .getOrElse { it.liftError() }
 }
 
 fun idle() = VMState.Idle
 fun loading() = VMState.Loading
 fun <T : Any> success(data: T) = VMState.Success(data)
 fun failed(e: Exception) = VMState.Failed(e)
+// endregion
+
+sealed interface VMState<out T : Any> {
+    object Idle : VMState<Nothing>
+    object Loading : VMState<Nothing>
+    data class Success<T : Any>(val data: T) : VMState<T>
+    data class Failed(val e: Exception) : VMState<Nothing>
+}
